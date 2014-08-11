072c660ed1c746a466b3e4805c98aa9c86dc137f
Brendan Berg <bergeoisie@gmail.com>
2014-08-11 07:16:43 -0400

    Added homom check

--- src/main/java/edu/umd/math/TextileChecks.java
+++ src/main/java/edu/umd/math/TextileChecks.java
@@ -1,5 +1,11 @@
 package edu.umd.math;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import org.jgrapht.graph.DirectedPseudograph;
+
 public class TextileChecks {
 
 	/*
@@ -7,10 +13,43 @@ public class TextileChecks {
 	 public static boolean checkNondegeneracy(Graph<V,E> g) {
 	 
 	 }
+	
+	 */
 	 
-	 public static boolean checkHomomorphisms()
+	 public static boolean checkHomomorphisms(Textile t) {
+	 
+		 Map<String,GEdge> edgeMap = new HashMap<String,GEdge>();
+		 
+		 DirectedPseudograph<GammaVertex,GammaEdge> gamma = t.getGammaGraph();
+		 DirectedPseudograph<GVertex,GEdge> g = t.getGGraph();
+		 
+		 Set<GEdge> gEdges = g.edgeSet();
+		 Set<GammaEdge> gammaEdges = gamma.edgeSet();
+		 
+		 for(GEdge e : gEdges) {
+			 edgeMap.put(e.getName(), e);
+		 }
+		 
+		 for(GammaEdge e : gammaEdges) {
+			 GEdge pe = edgeMap.get(e.getPEHom());
+			 GEdge qe = edgeMap.get(e.getQEHom());
+			 
+			 if( !gamma.getEdgeSource(e).getPVHom().equals(pe.getSourceName()) ||
+					 !gamma.getEdgeSource(e).getQVHom().equals(qe.getSourceName()) ||
+					 !gamma.getEdgeTarget(e).getPVHom().equals(pe.getTargetName()) ||
+					 !gamma.getEdgeTarget(e).getQVHom().equals(qe.getTargetName()) ) {
+				 System.out.println("There is a problem with " + e.getName() + " from " +
+					 e.getSourceName() + " to " + e.getTargetName() );
+				 
+				 return false;
+			 }
+		 }
+		 
+		 return true;
+	 
+	 }
 	 
 	 
-	*/
+	
 	
 }
